ccd54acf69ab7caebfe23e3157fdcff6a8eb3bb3
saritha9652 <84893812+saritha9652@users.noreply.github.com>
2021-06-01 11:48:30 +0530

    Update EmployeeSpringBootApplication.java

--- src/main/java/com/hcl/EmployeeSpringBootApplication.java
+++ src/main/java/com/hcl/EmployeeSpringBootApplication.java
@@ -2,12 +2,20 @@ package com.hcl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeSpringBootApplication {
+public class EmployeeSpringBootApplication extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeSpringBootApplication.class, args);
 	}
+	 @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(EmployeeSpringBootApplication.class);
+    }
+
 
 }
